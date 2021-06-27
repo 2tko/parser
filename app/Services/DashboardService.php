23f102d11856ab1115943d4446cab77c825ddde2
ilya_tutko <ilyat@getblend.com>
2021-06-27 14:26:59 +0300

    refactoring dashboard

--- app/Services/DashboardService.php
+++ app/Services/DashboardService.php
@@ -13,35 +13,43 @@ class DashboardService
 
     public function getData(string $orderType = 'asc'): array
     {
+        $between = [1, 20];
+        if ($orderType == 'asc') {
+            $maxRating = Project::max('rating');
+            $between = [$maxRating - 20, $maxRating];
+        }
         $projects = Project::query()
             ->from('projects as p')
             ->addSelect('p.id')
             ->addSelect('p.name')
-            ->join('projects_growth as pg', function ($join) {
-                $join->on('pg.project_id', '=', 'p.id')
-                    ->where('pg.key', '=', ProjectGrowth::TWO_WEEKS);
-            })
-            ->orderBy('pg.value', $orderType)
-            ->limit(self::LIMIT)
-            ->get();
+            ->addSelect('h.count')
+            ->addSelect('h.date')
+            ->join('holders as h', 'h.project_id', '=', 'p.id')
+            ->whereBetween('p.rating', $between)
+            ->where('h.date', '>=', Carbon::now()->subMonth()->toDateString())
+            ->orderBy('h.date')
+            ->get()
+            ->groupBy('name');
 
-        $holders = Holder::whereIn('project_id', $projects->pluck('id'))
-            ->where('date', '>=', Carbon::now()->subMonth()->toDateString())
-            ->orderBy('date')
-            ->get();
-
-        $i = 0;
         $data = [];
-        foreach ($projects as $project) {
-            $monthAgo = Carbon::now()->subMonth();
-            $start = 0;
-            $data[$i]['name'] = $project->name;
+        $i = 0;
+        foreach ($projects as $key => $projectData) {
+            $data[$i]['name'] = $key;
 
+            $monthAgo = Carbon::now()->subMonth();
+            $date = $monthAgo->toDateString();
+            $start = 0;
             while ($start <= 30) {
-                $date = $monthAgo->addDay()->toDateString();
-                $holder = $holders->where('project_id', $project->id)->where('date', $date)->first();
-                $data[$i]['data'][] = [$date, $holder->count ?? 0];
+                $count = 0;
+                $dataForDate = $projectData->where('date', $monthAgo->toDateString())->first();
+                if ($dataForDate) {
+                    $count = $dataForDate->count;
+                }
+
+                $data[$i]['data'][] = [$date, $count];
                 $start++;
+                $date = $monthAgo->addDay()->toDateString();
+
             }
 
             $i++;
@@ -61,8 +69,7 @@ class DashboardService
                 $join->on('pg.project_id', '=', 'p.id')
                     ->where('pg.key', '=', ProjectGrowth::TWO_WEEKS);
             })
-            ->orderBy('pg.value', $orderType)
-            ->limit(self::LIMIT)
+            ->whereBetween('p.rating', [1, 20])
             ->get();
 
         $data = [];
--- resources/js/components/CountHoldersDashboardComponent.vue
+++ resources/js/components/CountHoldersDashboardComponent.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container" style="padding: 100px;">
-        <highcharts v-loading="loading" :options="growth"></highcharts>
-        <highcharts v-loading="loading" :options="fall"></highcharts>
-        <highcharts v-loading="loading" :options="growthPercent"></highcharts>
+        <highcharts v-loading="loadingGrowth" :options="growth"></highcharts>
+        <highcharts v-loading="loadingFall" :options="fall"></highcharts>
+        <highcharts v-loading="loadingGrowthPercent" :options="growthPercent"></highcharts>
     </div>
 </template>
 
@@ -13,7 +13,9 @@ export default {
     name: "CountHoldersDashboardComponent",
     data() {
         return {
-            loading: false,
+            loadingGrowth: false,
+            loadingFall: false,
+            loadingGrowthPercent: false,
             growth : {
                 title: {
                     text: 'TOП 20 по РОСТУ холдеров в абсолютном значении'
@@ -77,25 +79,25 @@ export default {
     },
     methods: {
         getGrowthDashboardData() {
-            this.loading = true;
+            this.loadingGrowth = true;
             axios.get('/growth/count-holders-dashboard', {}).then(response => {
-                this.loading = false;
+                this.loadingGrowth = false;
                 this.growth.series = response.data.growth;
                 this.growth.xAxis.categories = response.data.categories;
             });
         },
         getFallDashboardData() {
-            this.loading = true;
+            this.loadingFall = true;
             axios.get('/fall/count-holders-dashboard', {}).then(response => {
-                this.loading = false;
+                this.loadingFall = false;
                 this.fall.series = response.data.fall;
                 this.fall.xAxis.categories = response.data.categories;
             });
         },
         getGrowthPercentDashboardData() {
-            this.loading = true;
+            this.loadingGrowthPercent = true;
             axios.get('/growth-percent/count-holders-dashboard', {}).then(response => {
-                this.loading = false;
+                this.loadingGrowthPercent = false;
                 this.growthPercent.series[0].data = response.data.growthPercent;
             });
         },
